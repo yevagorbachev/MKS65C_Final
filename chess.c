79597c9432d54e9da53997d40f13f2d1b234c21f
Bryan Lai <blai00@stuy.edu>
2020-01-10 15:33:05 -0500

    Render board

--- chess.c
+++ chess.c
@@ -1,28 +1,65 @@
 #include "chess.h"
 
-void setupBoard(struct chessboard c) {
+struct chessboard setupBoard(struct chessboard c) {
+	//Clears board and sets each empty position to '_';
+	for(int x = 0; x < 8; x++) {
+		for(int y = 0; y < 8; y++) {
+			c.board[x][y] = '_';
+	//		printf("|%c|",c.board[x][y]);
+		}	
+	//	printf("\n");
+	}
+
+	//Place pieces:
+	//NOTE: white is Uppercase while black is lowercase.
+	//pawns
 	for(int pawns = 0; pawns < 8; pawns++) {
-		c.board[7][pawns] = 'P';
-		c.board[1][pawns] = 'p';
-	}	
+		//white
+		c.board[6][pawns] = 'p';
+		//black
+		c.board[1][pawns] = 'P';
+	}
+
+	//Setting up white field
+	c.board[7][0] = WROOK;
+	c.board[7][1] = WKNIGHT;
+	c.board[7][2] = WBISHOP;
+	c.board[7][3] = WQUEEN;
+	c.board[7][4] = WKING;
+	c.board[7][5] = WBISHOP;
+	c.board[7][6] = WKNIGHT;
+	c.board[7][7] = WROOK;	
+
+	//Setting up black field
+	c.board[0][0] = BROOK;
+	c.board[0][1] = BKNIGHT;
+	c.board[0][2] = BBISHOP;
+	c.board[0][3] = BQUEEN;
+	c.board[0][4] = BKING;
+	c.board[0][5] = BBISHOP;
+	c.board[0][6] = BKNIGHT;
+	c.board[0][7] = BROOK;	
+
+
+	return c;
 }
 
 void renderBoard(struct chessboard c) {
-	char boardStr[128];
-	int counter = 0;
+	printf("\n\n\n\n    a b c d e f g h\n\n");
+	int counter = 8;
 	for(int x = 0; x < 8; x++) {
+		printf("%d  ", counter);
+		counter--;
 		for(int y = 0; y < 8; y++) {
-			boardStr[counter] = c.board[x][y];
-			counter++;
+			printf("|%c",c.board[x][y]);
 		}
-		boardStr[counter] = '\n';
-		counter++;
+		printf("|\n");
 	}
-	printf("\n%s\n",boardStr);
+	printf("\n\n\n\n");
 }
 
 int main() {
 	struct chessboard c;
-	setupBoard(c);
+	c = setupBoard(c);
 	renderBoard(c);
 }
--- chess.h
+++ chess.h
@@ -9,9 +9,22 @@
 #include <sys/types.h>
 #include <sys/sem.h>
 
+#define WPAWN 'P'
+#define WROOK 'R'
+#define WKNIGHT 'N'
+#define WBISHOP 'B'
+#define WKING 'K'
+#define WQUEEN 'Q'
+#define BPAWN 'p'
+#define BROOK 'r'
+#define BKNIGHT 'n'
+#define BBISHOP 'b'
+#define BKING 'k'
+#define BQUEEN 'q'
+
 struct chessboard { char board[8][8]; char whitecastle; char blackcastle;};
 
 int isValid(char m[3]);
 int movePiece(char m[3]);
 void renderBoard(struct chessboard c);
-void setupBoard(struct chessboard c);
+struct chessboard setupBoard(struct chessboard c);
