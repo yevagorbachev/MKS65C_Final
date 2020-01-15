368cdef5e52a16e149e94a4d08ae992ecbd7ed4a
Bryan Lai <blai00@stuy.edu>
2020-01-15 16:44:10 -0500

    Added comms code to render

--- lib/game.c
+++ lib/game.c
@@ -2,7 +2,7 @@
 
 void move(struct board * game, char player) {
     // print board, prompt move
-    render_board(game);
+    renderBoard(game,player);
     char buffer[3];
     printf("Move (%c): ", player);
     // printf("waiting");
@@ -28,6 +28,7 @@ void run(int path, char color) {
     switch(color) {
         case WHITE:
             game = init_board();
+	    game = setupBoard(game);
             while(1) {
                 move(&game, WHITE);
                 write(path, &game, sizeof(struct board));
@@ -46,6 +47,74 @@ void run(int path, char color) {
 
 }
 
+struct board clearBoard(struct board c) {
+	//Clears board and sets each empty position to '_';
+	for(int x = 0; x < 8; x++) {
+		for(int y = 0; y < 8; y++) {
+			c.state[x][y] = '_';
+	//		printf("|%c|",c.board[x][y]);
+		}	
+	//	printf("\n");
+	}
+	
+	return c;
+}
+
+struct board setupBoard(struct board c) {
+	c = clearBoard(c);
+	//a       b       c       d       e       f       g       h  
+	char setup[8][8] = {
+	{BROOK  ,BKNIGHT,BBISHOP,BKING  , BQUEEN,BBISHOP,BKNIGHT,BROOK,}, // 8
+	{BPAWN  , BPAWN ,BPAWN , BPAWN ,BPAWN  , BPAWN  ,BPAWN  ,BPAWN   ,}, // 7
+	{'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,}, // 6
+	{'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,}, // 5
+	{'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,}, // 4
+	{'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,'_'   ,}, // 3
+	{WPAWN  , WPAWN ,WPAWN , WPAWN ,WPAWN  , WPAWN  ,WPAWN  ,WPAWN,}, // 2
+	{WROOK  ,WKNIGHT,WBISHOP,WQUEEN  , WKING,WBISHOP,WKNIGHT,WROOK,}, // 1
+	};
+
+
+	for (int i = 7; i > -1; i--) {
+		memcpy(c.state[7-i], setup[i], 8);
+	}
+
+	return c;
+}
+
+void renderBoard(struct board * c, char player) {
+	//If the char inputted is w, render board with y perspective. (vice versa)
+	if(player == 'w') {
+		printf("\n Note: [!] Black is uppercase, White is lowercase\n");
+		int counter = 8;
+		for(int x = 7; x > -1; x--) {
+			printf("%d  ", counter);
+			counter--;
+			for(int y = 7; y > -1; y--) {
+				printf("|%c",c->state[x][y]);
+			}
+			printf("|\n");
+		}
+		printf("\n    a b c d e f g h\n\n");
+		printf("\n\n");
+
+	}
+	else if(player == 'b') {
+		printf("\n Note: [!] Black is uppercase, White is lowercase\n");
+		int counter = 1;
+		for(int x = 0; x < 8; x++) {
+			printf("%d  ", counter);
+			counter++;
+			for(int y = 0; y < 8; y++) {
+				printf("|%c",c->state[x][y]);
+			}
+			printf("|\n");
+		}
+		printf("\n    h g f e d c b a\n\n");
+		printf("\n\n");
+	}
+}
+
 struct board init_board() {
     struct board game;
     for(int i = 0; i < 8; i++) {
@@ -54,19 +123,4 @@ struct board init_board() {
         }
     }
     return game;
-}
-
-void render_board(struct board * game) {
-    printf("  ");
-    for (int i = 0; i < 8; i++) {
-        printf(" %c ", 'a' + i);
-    }
-    printf("\n");
-    for(int i = 0; i < 8; i++) {
-        printf("%d ", 8 - i);
-        for(int j = 0; j < 8; j++) {
-            printf(" %c ", game->state[i][j]);
-        }
-        printf("\n");
-    }
 }
--- lib/game.h
+++ lib/game.h
@@ -3,12 +3,26 @@
 #define WHITE 'w'
 #define BLACK 'b'
 
-struct board {
-    char state[8][8];
-    char wcas, blcas;
-};
+#define WPAWN 'f'
+#define WROOK 'r'
+#define WKNIGHT 'n'
+#define WBISHOP 'b'
+#define WKING 'k'
+#define WQUEEN 'q'
+#define BPAWN 'F'
+#define BROOK 'R'
+#define BKNIGHT 'N'
+#define BBISHOP 'B'
+#define BKING 'K'
+#define BQUEEN 'Q'
 
+struct board { char state[8][8]; char wcas; char blcas;};
+
+int isValid(char m[3]);
+int movePiece(char m[3]);
+void renderBoard(struct board * c, char player);
+struct board setupBoard(struct board c);
+struct board clearBoard(struct board c);
 void run(int path, char color);
 void move(struct board * game, char player);
 struct board init_board();
-void render_board(struct board * state);
