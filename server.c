afd4a5d30828ff09eb3bdbd984f08c0cb737bcde
Amanda Chen <achen01@stuy.edu>
2020-01-16 15:20:01 -0500

    added ip to client

--- server.c
+++ server.c
@@ -18,8 +18,13 @@ int game_connect(char mode, char * channel) {
 	return path;
 }
 
-int main() {
-	int path = game_connect(MODE_NET, LOCALHOST);
+int main(int argc, char * argv[]) {
+	int path;
+	if (argc > 1){
+		path = game_connect(MODE_NET, argv[1]);
+	}else{
+		path = game_connect(MODE_NET, LOCALHOST);
+	}
 	char color = '\0';
 
 	char name[256];
