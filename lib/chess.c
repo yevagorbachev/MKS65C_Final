ea70ce4e3901d8241729afc8f51a2557a95861d8
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2020-01-15 19:28:19 -0500

    pins in progress

--- lib/chess.c
+++ lib/chess.c
@@ -1,5 +1,7 @@
 #include "chess.h"
 
+// HELPER FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 char chk_color(struct board * board, struct coordinate pos) {
     char piece = BOARD(pos);
     if (piece == '\0') {
@@ -14,13 +16,11 @@ char chk_color(struct board * board, struct coordinate pos) {
 char on_board(struct coordinate pos) {
     return (('1' <= pos.rank) && (pos.rank <= '8') && ('a' <= pos.file) && (pos.file <= 'h'));
 }
-
 void add_move(struct moves * moves, struct coordinate origin, struct coordinate move) {
     int i = 0;
     while (MOVES(move)[i++].file != '\0'); // go to end
     MOVES(move)[i - 1] = origin;
 }
-
 void print_moves(struct moves * moves, struct coordinate move) {
     int i = 0;        
     printf("Possible coordinates to move to %c%c from:\n", CHARGS(move));
@@ -29,17 +29,17 @@ void print_moves(struct moves * moves, struct coordinate move) {
         i++;
     }
 }
-
 struct moves * init_moves() {
     return calloc(sizeof(struct moves), 1);
 }
-
 struct coordinate coord(char * coord) {
 	struct coordinate square;
 	square.file = coord[0];
 	square.rank = coord[1];
 	return square;
 }
+
+// LOGIC ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 int pin(struct board * board, struct moves * moves, struct coordinate origin, int * diagonal) {
     char own_color = chk_color(board, origin);
@@ -91,4 +91,37 @@ int pin(struct board * board, struct moves * moves, struct coordinate origin, in
     }
     // printf("exited pin\n");
 
+}
+
+int pins(struct board * board, struct moves * moves, struct coordinate origin) {
+    int diagonals[4][2] = {
+        {0,0},
+        {0,0},
+        {0,0},
+        {0,0},
+    };
+    char piece = BOARD(origin);
+    // set relevant diagonals according to piece
+    if ((piece == WROOK) | (piece == BROOK)) {
+        diagonals[0][0] = 1;
+        diagonals[1][1] = 1;
+    }
+    else if ((piece == WBISHOP) | (piece == BBISHOP)) {
+        diagonals[0][0] = 1;
+        diagonals[0][1] = 1;
+        diagonals[1][0] = 1;
+        diagonals[1][0] = -1;
+    }
+    else if ((piece == WQUEEN) | (piece == BQUEEN)) {
+        diagonals[0][0] = 1;
+        diagonals[1][1] = 1;
+        diagonals[2][0] = 1;
+        diagonals[2][1] = 1;
+        diagonals[3][0] = 1;
+        diagonals[3][0] = -1;
+    }
+    int checks;
+
+    for (int i = 0; i < 4 && (diagonals[i][0] + diagonals[i][1]); i++) {
+    }
 }
